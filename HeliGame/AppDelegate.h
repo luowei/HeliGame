79b434427a76c57a3cf09e4b8d3c0084302895ac
luowei <luowei010101@gmail.com>
2014-06-03 10:04:10 +0800

    Initial Commit

---
+++ HeliGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HeliGame
+//
+//  Created by luowei on 14-6-3.
+//  Copyright (c) 2014年 rootls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
